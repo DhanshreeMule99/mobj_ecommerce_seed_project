4d3e56d26d60cc06b898af1233e87f9f0b214f51
vaishnavidubbalwar2001 <vaishanvi@setoo.co>
2024-04-17 16:06:12 +0530

    added: deactivate account feature

--- lib/services/shopifyServices/restAPIServices/profile/logoutRepository.dart
+++ lib/services/shopifyServices/restAPIServices/profile/logoutRepository.dart
@@ -1,4 +1,6 @@
 // logoutRepository
+import 'package:dio/dio.dart';
+import 'package:mobj_project/utils/api.dart';
 import 'package:mobj_project/utils/cmsConfigue.dart';
 
 class logoutRepository {
@@ -6,7 +8,32 @@ class logoutRepository {
 
   Future<bool> accountDeactivate() async {
 
-    if (await ConnectivityUtils.isNetworkConnected()) {
+API api= API();
+if(AppConfigure.bigCommerce){
+if (await ConnectivityUtils.isNetworkConnected()) {
+ 
+      final uid = await SharedPreferenceManager().getUserId();
+      final response = await api.sendRequest.delete(
+          "/customers?id:in=$uid",
+          options: Options(headers: {
+              "X-auth-Token": "${AppConfigure.bigCommerceAccessToken}",
+            
+            }) );
+      // var data = response.data;
+      if (response.statusCode == 204) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+
+}
+
+else
+
+   { if (await ConnectivityUtils.isNetworkConnected()) {
       String BASE_URL = AppConfigure.baseUrl +
           APIConstants.apiForAdminURL +
           APIConstants.apiURL +
@@ -22,6 +49,7 @@ class logoutRepository {
       }
     } else {
       return false;
+    }
     }
   }
 }
