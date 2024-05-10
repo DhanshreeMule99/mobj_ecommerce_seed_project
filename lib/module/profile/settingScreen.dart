277bcfe6965492f03f48ed52abf6c4c4ff026654
vaishnavidubbalwar2001 <vaishanvi@setoo.co>
2024-05-10 17:23:54 +0530

    add(setting): added new UI for setting screen

--- lib/module/profile/settingScreen.dart
+++ lib/module/profile/settingScreen.dart
@@ -1,4 +1,5 @@
 // SettingScreen
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:mobj_project/utils/cmsConfigue.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -210,6 +211,14 @@ class SettingScreenState extends ConsumerState<SettingScreen> {
     return appInfoAsyncValue.when(
         data: (appInfo) => Scaffold(
               appBar: AppBar(
+                  leading: IconButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              icon: Icon(
+                Icons.chevron_left_rounded,
+                size: 25.sp,
+              )),
                 elevation: 2,
                 title: Text(
                   AppLocalizations.of(context)!.setting,
@@ -241,11 +250,25 @@ class SettingScreenState extends ConsumerState<SettingScreen> {
                     },
                     title:
                         '${AppLocalizations.of(context)!.about} ${appInfo.appName}',
-                    leading: const Icon(Icons.info),
+                    // leading: const Icon(Icons.info),
+                    trailing:  Icon(Icons.arrow_forward_ios,
+                      color: Theme.of(context).colorScheme.primary,
+                      size: 20,
+                      ) ,
+                      icon: Icons.info,
+                            iconColor:  Theme.of(context).colorScheme.primary,
+                          backgroundColor: Theme.of(context).colorScheme.onPrimary,
                   ),
                   appInfo.tawkURL != "" || appInfo.tawkURL.isNotEmpty
                       ? MobjListTile(
-                          leading: const Icon(Icons.help),
+                        trailing:  Icon(Icons.arrow_forward_ios,
+      color: Theme.of(context).colorScheme.primary,
+      size: 20,
+      ) ,
+                          // leading: const Icon(Icons.help),
+                            icon: Icons.help,
+                            iconColor:  Theme.of(context).colorScheme.primary,
+                          backgroundColor: Theme.of(context).colorScheme.onPrimary,
                           title: AppLocalizations.of(context)!.help,
                           onTap: () {
                             Navigator.of(context).push(
@@ -261,7 +284,14 @@ class SettingScreenState extends ConsumerState<SettingScreen> {
                         )
                       : const SizedBox(),
                   MobjListTile(
-                    leading: const Icon(Icons.vpn_key),
+                    trailing:  Icon(Icons.arrow_forward_ios,
+      color: Theme.of(context).colorScheme.primary,
+      size: 20,
+      ) ,
+                    // leading: const Icon(Icons.vpn_key),
+                      icon: Icons.vpn_key,
+                            iconColor:  Theme.of(context).colorScheme.primary,
+                          backgroundColor: Theme.of(context).colorScheme.onPrimary,
                     title: AppLocalizations.of(context)!.resetPass,
                     onTap: () async {
                       Navigator.of(context).push(
@@ -277,9 +307,19 @@ class SettingScreenState extends ConsumerState<SettingScreen> {
                     },
                   ),
                   MobjListTile(
-                    leading: const Icon(Icons.lightbulb_outline),
+                
+                  icon: Icons.lightbulb_outline,
+                            iconColor:  Theme.of(context).colorScheme.primary,
+                          backgroundColor: Theme.of(context).colorScheme.onPrimary,
                     title: AppLocalizations.of(context)!.themeChange,
-                    trailing: Switch(
+                    trailing:
+                     Switch(
+                      trackOutlineColor:  MaterialStatePropertyAll( Theme.of(context).colorScheme.primary), 
+                      inactiveThumbColor: Theme.of(context).colorScheme.primary ,
+                      inactiveTrackColor: Theme.of(context).colorScheme.onPrimary,
+                       activeTrackColor: Theme.of(context).colorScheme.onPrimary,
+                        focusColor: Theme.of(context).colorScheme.onPrimary ,
+                      activeColor: Theme.of(context).colorScheme.primary,
                       value: theme,
                       onChanged: (value) =>
                           ref.read(themeProvider.notifier).toggleTheme(),
@@ -287,7 +327,17 @@ class SettingScreenState extends ConsumerState<SettingScreen> {
                     onTap: () {},
                   ),
                   ListTile(
-                    leading: const Icon(Icons.language),
+                     leading: 
+                     CircleAvatar(
+                        radius: 15,
+                        backgroundColor:Theme.of(context).colorScheme.onPrimary ,
+                       child: Icon(Icons.language,
+                       size: 20,
+                       color: Theme.of(context).colorScheme.primary,)),
+                        trailing:  Icon(Icons.arrow_forward_ios,
+                        color: Theme.of(context).colorScheme.primary,
+                        size: 20,
+                        ),
                     title: Text(AppLocalizations.of(context)!.changeLanguage),
                     onTap: () {
                       _showLanguageDialog(context, languageProviders);
@@ -308,7 +358,13 @@ class SettingScreenState extends ConsumerState<SettingScreen> {
                   //   },
                   // ),
                   MobjListTile(
-                    leading: const Icon(Icons.info),
+                    trailing:  Icon(Icons.arrow_forward_ios,
+      color: Theme.of(context).colorScheme.primary,
+      size: 20,
+      ) ,
+                    icon: Icons.info,
+                            iconColor:  Theme.of(context).colorScheme.primary,
+                          backgroundColor: Theme.of(context).colorScheme.onPrimary,
                     title: AppLocalizations.of(context)!.privacyPolicy,
                     onTap: () {
                       Navigator.of(context).push(
@@ -322,7 +378,13 @@ class SettingScreenState extends ConsumerState<SettingScreen> {
                     },
                   ),
                   MobjListTile(
-                    leading: const Icon(Icons.star),
+                    trailing:  Icon(Icons.arrow_forward_ios,
+                      color: Theme.of(context).colorScheme.primary,
+                      size: 20,
+                      ) ,
+                    icon: Icons.star,
+                            iconColor:  Theme.of(context).colorScheme.primary,
+                          backgroundColor: Theme.of(context).colorScheme.onPrimary,
                     title: AppLocalizations.of(context)!.rateUs,
                     onTap: () async {
                       LaunchReview.launch(
@@ -330,21 +392,34 @@ class SettingScreenState extends ConsumerState<SettingScreen> {
                           iOSAppId: "com.example.mobjProject");
                     },
                   ),
-                  MobjListTile(
-                    leading: const Icon(Icons.logout),
-                    title: AppLocalizations.of(context)!.logout,
-                    onTap: () async {
-                      bool? logoutConfirmed =
-                          await showLogoutConfirmationDialog(context);
-                      if (logoutConfirmed!) {
-                        CommonAlert.show_loading_alert(context);
-                        logout();
-                      }
-                      // Remove loginId from shared preferences
-                    },
-                  ),
+                  // MobjListTile(
+                  //   leading: const Icon(Icons.logout),
+                  //   title: AppLocalizations.of(context)!.logout,
+                  //   onTap: () async {
+                  //     bool? logoutConfirmed =
+                  //         await showLogoutConfirmationDialog(context);
+                  //     if (logoutConfirmed!) {
+                  //       CommonAlert.show_loading_alert(context);
+                  //       logout();
+                  //     }
+                  //     // Remove loginId from shared preferences
+                  //   },
+                  // ),
                   ListTile(
-                      leading: const Icon(Icons.person),
+                    // icon: Icons.person,
+                            // iconColor:  Theme.of(context).colorScheme.primary,
+                          // backgroundColor: Theme.of(context).colorScheme.onPrimary,
+                       leading: CircleAvatar(
+                        radius: 15,
+                        backgroundColor:Theme.of(context).colorScheme.onPrimary ,
+                       child: Icon(Icons.person,
+                       size: 20,
+                       
+                       color: Theme.of(context).colorScheme.primary,)),
+                        trailing:  Icon(Icons.arrow_forward_ios,
+                        color: Theme.of(context).colorScheme.primary,
+                         size: 20,
+                         ),
                       title: Text(AppLocalizations.of(context)!.accDeactivate),
                       onTap: () async {
                         bool? logoutConfirmed =
@@ -498,4 +573,49 @@ Future<bool?> showDeactivateAccountDialog(BuildContext context) {
       );
     },
   );
+}
+
+
+class MobjListTile extends StatelessWidget {
+  final GestureTapCallback onTap;
+  final String title;
+   final IconData icon;
+  final Color iconColor;
+  final Color backgroundColor;
+  final Widget? trailing;
+  MobjListTile({
+    Key? key,
+    required this.onTap,
+    required this.title,  required this.icon,
+    required this.iconColor,
+    required this.backgroundColor,  this.trailing,
+  }) : super(key: key);
+
+ 
+// class _MobjListTiletate extends State<MobjListTile> {
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+        leading: Container(
+        width: 30,
+        height: 30,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: backgroundColor,
+        ),
+        child: Icon(
+          size: 20,
+          icon,
+          color: iconColor,
+        ),
+      ),
+        title: Text(title),
+         trailing:  trailing,
+      //    Icon(Icons.arrow_forward_ios,
+      // color: Theme.of(context).colorScheme.primary,
+      // size: 20,
+      // ) ,
+        onTap: onTap);
+  }
+
 }
--- lib/utils/listTile.dart
+++ lib/utils/listTile.dart
@@ -1,27 +1,43 @@
 import 'package:flutter/material.dart';
 
-class MobjListTile extends StatefulWidget {
-  final GestureTapCallback onTap;
-  final String title;
-  final Widget? leading;
-final Widget? trailing;
-  const MobjListTile({
-    Key? key,
-    required this.onTap,
-    required this.title,  this.leading, this.trailing,
-  }) : super(key: key);
+// class MobjListTile extends StatefulWidget {
+//   final GestureTapCallback onTap;
+//   final String title;
+//    final IconData icon;
+//   final Color iconColor;
+//   final Widget backgroundColor;
+//   final Widget? trailing;
+//   const MobjListTile({
+//     Key? key,
+//     required this.onTap,
+//     required this.title,  required this.icon,
+//     required this.iconColor,
+//     required this.backgroundColor, this.trailing,
+//   }) : super(key: key);
 
-  @override
-  State<MobjListTile> createState() => _MobjListTiletate();
-}
+//   @override
+//   State<MobjListTile> createState() => _MobjListTiletate();
+// }
 
-class _MobjListTiletate extends State<MobjListTile> {
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-        leading: widget.leading,
-        title: Text(widget.title),
-        trailing: widget.trailing,
-        onTap: widget.onTap);
-  }
-}
+// class _MobjListTiletate extends State<MobjListTile> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return ListTile(
+//         leading: Container(
+//         width: 30,
+//         height: 30,
+//         decoration: BoxDecoration(
+//           shape: BoxShape.circle,
+//           color: backgroundColor,
+//         ),
+//         child: Icon(
+//           size: 20,
+//           icon,
+//           color: iconColor,
+//         ),
+//       ),
+//         title: Text(widget.title),
+//         trailing: widget.trailing,
+//         onTap: widget.onTap);
+//   }
+// }
