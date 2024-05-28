d095ba1192de46e2515109ca8da275063ebba826
vaishnavidubbalwar2001 <vaishanvi@setoo.co>
2024-05-28 16:07:43 +0530

    privacy Screen text by API

--- lib/module/profile/privacyScreen.dart
+++ lib/module/profile/privacyScreen.dart
@@ -1,9 +1,16 @@
-// privacyScreen
+
+
+
+
+import 'dart:convert';
 import 'package:amazon_like_filter/props/applied_filter_model.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:mobj_project/utils/cmsConfigue.dart';
 import 'package:http/http.dart' as http;
+import 'package:mobj_project/utils/cmsConfigue.dart';
+
 import '../wishlist/wishlishScreen.dart';
 
 class PrivacyScreen extends ConsumerStatefulWidget {
@@ -16,7 +23,17 @@ class PrivacyScreen extends ConsumerStatefulWidget {
 }
 
 class _PrivacyScreenState extends ConsumerState<PrivacyScreen> {
- 
+  Future<String> fetchTermsAndConditions() async {
+    final response = await http.get(Uri.parse(
+        'https://c0a6-2409-40c2-1196-1baf-c01d-6b7-511c-6c.ngrok-free.app/api/terms-and-condition'));
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      return data['data']['attributes']['terms_and_condition'];
+    } else {
+      throw Exception('Failed to load terms and conditions');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -58,103 +75,159 @@ class _PrivacyScreenState extends ConsumerState<PrivacyScreen> {
                   data: (user) {
                     return SingleChildScrollView(
                         child: Center(
-                      child: ref.watch(productDataProvider('1')).when(
-                            data: (profile) {
-                              return Padding(
-                                padding: const EdgeInsets.all(10),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: <Widget>[
-                                    Center(
-                                      child: ClipRRect(
-                                          child: CachedNetworkImage(
-                                        imageUrl: appInfo.logoImagePath,
-                                        imageBuilder:
-                                            (context, imageProvider) =>
-                                                Container(
-                                          width:
-                                              MediaQuery.of(context).size.width,
-                                          height: MediaQuery.of(context)
-                                                  .size
-                                                  .height *
-                                              0.2,
-                                          decoration: BoxDecoration(
-                                            image: DecorationImage(
-                                              //image size fill
-                                              image: imageProvider,
-                                              fit: BoxFit.contain,
-                                            ),
+                            child: ref.watch(productDataProvider('1')).when(
+                                  data: (profile) {
+                                    return Padding(
+                                      padding: const EdgeInsets.all(10),
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: <Widget>[
+                                          Center(
+                                            child: ClipRRect(
+                                                child: CachedNetworkImage(
+                                              imageUrl: appInfo.logoImagePath,
+                                              imageBuilder:
+                                                  (context, imageProvider) =>
+                                                      Container(
+                                                width: MediaQuery.of(context)
+                                                    .size
+                                                    .width,
+                                                height: MediaQuery.of(context)
+                                                        .size
+                                                        .height *
+                                                    0.2,
+                                                decoration: BoxDecoration(
+                                                  image: DecorationImage(
+                                                    //image size fill
+                                                    image: imageProvider,
+                                                    fit: BoxFit.contain,
+                                                  ),
+                                                ),
+                                              ),
+                                              placeholder: (context, url) =>
+                                                  Container(
+                                                width: 28,
+                                                height: 28,
+                                                color: Colors.grey.shade800,
+                                              ),
+                                              errorWidget:
+                                                  (context, url, error) =>
+                                                      Container(
+                                                width: 28,
+                                                height: 28,
+                                                color: Colors.grey.shade800,
+                                              ),
+                                            )),
                                           ),
+                                          const SizedBox(height: 20),
+                                          FutureBuilder<String>(
+                                            future: fetchTermsAndConditions(),
+                                            builder: (context, snapshot) {
+                                              if (snapshot.connectionState ==
+                                                  ConnectionState.waiting) {
+                                                return const SkeletonLoaderWidget();
+                                              } else if (snapshot.hasError) {
+                                                return Column(
+                                                  mainAxisAlignment:
+                                                      MainAxisAlignment.center,
+                                                  crossAxisAlignment:
+                                                      CrossAxisAlignment.center,
+                                                  children: [
+                                                    const Center(
+                                                      child: ErrorHandling(
+                                                        error_type:
+                                                            AppString.error,
+                                                      ),
+                                                    ),
+                                                    ElevatedButton(
+                                                      style: ElevatedButton
+                                                          .styleFrom(
+                                                        backgroundColor:
+                                                            AppColors
+                                                                .buttonColor,
+                                                      ),
+                                                      onPressed: () {
+                                                        ref.refresh(
+                                                            userDataProvider);
+                                                      },
+                                                      child: Text(
+                                                        AppLocalizations.of(
+                                                                context)!
+                                                            .refresh,
+                                                        style: const TextStyle(
+                                                          fontSize: 16,
+                                                          color: AppColors
+                                                              .whiteColor,
+                                                        ),
+                                                      ),
+                                                    )
+                                                  ],
+                                                );
+                                              } else {
+                                                return Text(
+                                                  snapshot.data!,
+                                                  style: Theme.of(context)
+                                                      .textTheme
+                                                      .headlineMedium,
+                                                );
+                                              }
+                                            },
+                                          ),
+                                        ],
+                                      ),
+                                    );
+                                  },
+                                  loading: () =>
+                                      const SkeletonLoaderWidget(),
+                                  error: (error, stackTrace) => Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      SizedBox(
+                                        height: MediaQuery.of(context)
+                                                .size
+                                                .height /
+                                            4.2,
+                                      ),
+                                      const Center(
+                                        child: ErrorHandling(
+                                          error_type: AppString.error,
                                         ),
-                                        placeholder: (context, url) =>
-                                            Container(
-                                          width: 28,
-                                          height: 28,
-                                          color: Colors.grey.shade800,
-                                        ),
-                                        errorWidget: (context, url, error) =>
-                                            Container(
-                                          width: 28,
-                                          height: 28,
-                                          color: Colors.grey.shade800,
-                                        ),
-                                      )),
-                                    ),
-                                    const SizedBox(height: 20),
-                                    const SizedBox(height: 20),
-                                    Text(
-                                      "Setoo recognises the importance of maintaining your privacy. We value your privacy and appreciate your trust in us. This Privacy Policy sets out how Setoo uses and protects any information that you give Setoo when you use this www.setoo.co or the Setoo mobile application or any other digital medium and other offline sources of our Company. Setoo is committed to ensure that your privacy is protected. Should we ask you to provide certain information by which you can be identified when using this website, then you can be assured that it will only be used in accordance with this Privacy Policy as it describes how we treat user information we collect from you, the policies and procedures on the collection, use, disclosure and protection of your information when you use our Setoo Platform.",
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .headlineMedium,
-                                    ),
-                                  ],
-                                ),
-                              );
-                            },
-                            loading: () => const SkeletonLoaderWidget(),
-                            error: (error, stackTrace) => Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                SizedBox(
-                                  height:
-                                      MediaQuery.of(context).size.height / 4.2,
-                                ),
-                                const Center(
-                                  child: ErrorHandling(
-                                    error_type: AppString.error,
+                                      ),
+                                      Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        children: [
+                                          const Center(
+                                            child: ErrorHandling(
+                                                error_type: AppString.error),
+                                          ),
+                                          ElevatedButton(
+                                            style: ElevatedButton.styleFrom(
+                                              backgroundColor:
+                                                  AppColors.buttonColor,
+                                            ),
+                                            onPressed: () {
+                                              ref.refresh(userDataProvider);
+                                            },
+                                            child: Text(
+                                              AppLocalizations.of(context)!
+                                                  .refresh,
+                                              style: const TextStyle(
+                                                fontSize: 16,
+                                                color: AppColors.whiteColor,
+                                              ),
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ],
                                   ),
-                                ),
-                                Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: [
-                                    const Center(
-                                      child: ErrorHandling(
-                                          error_type: AppString.error),
-                                    ),
-                                    ElevatedButton(
-                                      style: ElevatedButton.styleFrom(
-                                        backgroundColor: AppColors.buttonColor,
-                                      ),
-                                      onPressed: () {
-                                        ref.refresh(userDataProvider);
-                                      },
-                                      child: Text(
-                                        AppLocalizations.of(context)!.refresh,
-                                        style: const TextStyle(
-                                          fontSize: 16,
-                                          color: AppColors.whiteColor,
-                                        ),
-                                      ),
-                                    )
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                    ));
+                                )));
                   },
                   error: (error, s) =>
                       const ErrorHandling(error_type: AppString.error),
